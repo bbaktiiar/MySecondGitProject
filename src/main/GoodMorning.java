c2d05abedf41307b63577643a10d1f0cac94362a
baktiiarbaitemirov <baktiiarbaitemirov@10.1.10.193>
2018-03-10 11:24:20 -0600

    last update

--- src/main/GoodMorning.java
+++ src/main/GoodMorning.java
@@ -3,7 +3,7 @@ package main;
 public class GoodMorning {
 	public static void main(String[] args) {
 		System.out.println("Good morning class!");
-		System.out.println("My name is Baktiiar");
+		System.out.println("update");
 	}
 
 }
